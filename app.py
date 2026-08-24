2e2c819b71a09900a3ff40911ff3783a875b3f66
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-08-24 11:46:24 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, flash #Flash é utilizado para dar alertas ao usuário na tela
+# Flash é utilizado para dar alertas ao usuário na tela
+from flask import Flask, render_template, request, redirect, flash
 import mysql.connector
-import werkzeug.security as security # Biblioteca que será utilizada para criptografar a senha do usuário
 
 app = Flask(__name__)
+app.secret_key = "sabonete"
 
 bd_config = {
     'host': 'localhost',
@@ -12,38 +13,52 @@ bd_config = {
     'ssl_disabled': True
 }
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
-@app.route("/cadastro_usuario", methods="POST")
+
+@app.route("/cadastro_usuario", methods=["GET", "POST"])
 def cadastroUser():
+    if request.method == "GET":
+        return render_template("cadastro.html")
+
     nome = request.form.get("nome")
     email = request.form.get("email")
     senha = request.form.get("senha")
     confirmar_senha = request.form.get("confirm-senha")
 
-    if senha != confirmar_senha:
-        flash()
+    if len(senha) < 8 or len(senha) > 20:
+        flash("A senha deve ter entre 8 e 20 caracteres.")
         return redirect('/cadastro_usuario')
 
-    senha_criptografada = security.generate_password_hash(senha)
+    if senha != confirmar_senha:
+        flash("As senhas não coincidem.")
+        return redirect('/cadastro_usuario')
+
     try:
 
         conexao = mysql.connector.connect(**bd_config)
         cursor = conexao.cursor(dictionary=True)
 
+        query = "INSERT INTO usuario (EMAIL, NOME, SENHA, ADM) VALUES (%s, %s, %s, %s)"
+        cursor.execute(query, (email, nome, senha, False))
+        conexao.commit()
+
         cursor.close()
         conexao.close()
-    except:
-        pass
-    finally:
-        pass
-    return render_template("cadastro.html")
+    except mysql.connector.Error as err:
+        return f"Erro ao gravar no Banco: {err}"
+
+    flash("Cadastro realizado com sucesso! Faça login para continuar.")
+    return redirect("/login")
+
 
 @app.route("/restaurantes", methods=["GET", "POST"])
 def restaurantes():
@@ -67,9 +82,10 @@ def restaurantes():
     conexao.close()
     return render_template("restaurantes_user.html", restaurantes=restaurantes, termo=termo)
     # if (usuario == user):
-    #     
+    #
     # elif (usuario == admin):
     #     return render_template("restaurantes_admin.html", restaurantes=restaurantes, termo=termo)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
